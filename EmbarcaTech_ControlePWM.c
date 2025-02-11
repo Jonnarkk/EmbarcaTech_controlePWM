8fd98f578ad3fce944d48ebdc1eb1294faca34d4
Miller <guimigc@protonmail.com>
2025-02-10 22:03:33 -0300

    rollback: readicionando código apagado por engano

--- EmbarcaTech_ControlePWM.c
+++ EmbarcaTech_ControlePWM.c
@@ -4,6 +4,7 @@
 
 // Defines
 #define SERVOMOTORPWM_PIN 22 // Pino do servomotor
+#define BLUE_PIN 12 // Pino do LED azul
 #define WRAP 65535 // Valor padrão do contador 16 bits
 #define MAIOR_ANGULO 7864 // Limite superior do servomotor
 #define MENOR_ANGULO 1638 // Limite inferior do servomotor
@@ -23,6 +24,19 @@ void pwm_inicial_setup(){
     pwm_init(slice, &config, true); // Inicializando o PWM
 }
 
+// Função para inicialização do LED como sinal PWM
+void pwm_led_setup(){
+    gpio_set_function(BLUE_PIN, GPIO_FUNC_PWM); // Habilita o pino GPIO como PWM
+
+    uint slice = pwm_gpio_to_slice_num(BLUE_PIN); // Obtém o canal PWM da GPIO
+
+    pwm_set_clkdiv(slice, 38.0); // Define o divisor de clock do PWM
+
+    pwm_set_wrap(slice, WRAP); // Define o valor de wrap
+
+    pwm_set_enabled(slice, true); // Habilita o pwm no slice correspondente
+
+}
 
 int main(){
     uint16_t angulo = 1638; // Variável para fazer a movimentação suave do servomotor
@@ -30,19 +44,24 @@ int main(){
 
     stdio_init_all();
     pwm_inicial_setup();
+    pwm_led_setup();
 
     pwm_set_gpio_level(SERVOMOTORPWM_PIN, 7864); // Ajuste do servo motor para 180°
+    pwm_set_gpio_level(BLUE_PIN, 7864); // Ajuste do servo motor para 180°
     printf("180 graus\n");
     sleep_ms(5000);
     pwm_set_gpio_level(SERVOMOTORPWM_PIN, 4817); // Ajuste do servo motor para 90°
+    pwm_set_gpio_level(BLUE_PIN, 4817); // Ajuste do servo motor para 90°
     printf("90 graus\n");
     sleep_ms(5000);
     pwm_set_gpio_level(SERVOMOTORPWM_PIN, 1638); // Ajuste do servo motor para 0°
+    pwm_set_gpio_level(BLUE_PIN, 1638); // Ajuste do servo motor para 0°
     printf("0 graus\n");
     sleep_ms(5000);
 
     while (true) {
         pwm_set_gpio_level(SERVOMOTORPWM_PIN, angulo); // Função para alterar o ciclo ativo do período
+        pwm_set_gpio_level(BLUE_PIN, angulo); // Função para alterar o ciclo ativo do período
         
         if(up_down){
             printf("Incrementando angulo: %d\n", angulo);
